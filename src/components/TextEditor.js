dc9700dda29a165573426518eddca78a80cd91b7
Madhuri Gubbala <gmadhuri@ucsd.edu>
2018-10-21 14:35:09 -0700

    Enable making text bold

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import {Editor} from 'slate-react'
 import {Value} from 'slate'
+import BoldMark from './BoldMark'
 
 const initialValue = Value.fromJSON({
     document: {
@@ -34,9 +35,37 @@ export default class TextEditor extends Component {
         this.setState({value})
     };
 
+    onKeyDown = (e, change) => {
+        if (!e.ctrlKey) {
+            return
+        }
+        e.preventDefault();
+
+        switch (e.key) {
+            case 'b': {
+                change.addMark("bold");
+                return true;
+            }
+            default:
+                return false;
+        }
+    };
+
+    renderMark = props => {
+        switch (props.mark.type) {
+            case 'bold':
+                return <BoldMark {...props} />
+        }
+    };
+
     render() {
         return (
-            <Editor value={this.state.value} onChange={this.onChange}/>
+            <Editor
+                value={this.state.value}
+                onChange={this.onChange}
+                onKeyDown={this.onKeyDown}
+                renderMark={this.renderMark}
+            />
         )
     }
 }
